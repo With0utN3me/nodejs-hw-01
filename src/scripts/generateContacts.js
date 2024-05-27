57918c90cf593dc7cb3dfe81e0ca381d9c0159be
With0utN3me <sinitsin678@gmail.com>
2024-05-27 21:42:46 +0200

    generate contacts ready

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    let contacts = [];
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        contacts = JSON.parse(data);
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+    for (let i = 0; i <= number; i++){
+        contacts.push(createFakeContact());
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    console.log(`${number} contacts generated and saved to ${PATH_DB}`);
+};
 
-await generateContacts(5);
+await generateContacts(3);
